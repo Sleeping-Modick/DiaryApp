ba88b2ffa3af0dd71bb01d5a57eb43def3528039
jakkujakku <jakkujakku98@gmail.com>
2023-08-29 15:43:02 +0900

    탭 바 연결 수정 및 View의 배경색상 설정

--- DiaryApp/Controllers/DiaryCalenderViewController.swift
+++ DiaryApp/Controllers/DiaryCalenderViewController.swift
@@ -10,5 +10,6 @@ import UIKit
 class DiaryCalenderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
     }
 }
--- DiaryApp/Controllers/ProfileViewController.swift
+++ DiaryApp/Controllers/ProfileViewController.swift
@@ -10,5 +10,6 @@ import UIKit
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
     }
 }
