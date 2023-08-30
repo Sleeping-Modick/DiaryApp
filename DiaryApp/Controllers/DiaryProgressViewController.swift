52e45b55d817827e1eb2136f5a59295b2a9b85ae
jakkujakku <jakkujakku98@gmail.com>
2023-08-30 19:18:09 +0900

    [FEAT]: DiaryProgressCell 생성

--- DiaryApp/Controllers/DiaryProgressViewController.swift
+++ DiaryApp/Controllers/DiaryProgressViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class DiaryProgressViewController: UIViewController {
+    private let collectionView = CustomCollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+    }
+}
+
+extension DiaryProgressViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
---
+++ DiaryApp/Views/Cells/DiaryProgressCell.swift
@@ -0,0 +1,10 @@
+//
+//  DiaryProgressCell.swift
+//  DiaryApp
+//
+//  Created by (^ㅗ^)7 iMac on 2023/08/30.
+//
+
+import Cocoa
+
+final class DiaryProgressCell: UICollectionViewCell {}
