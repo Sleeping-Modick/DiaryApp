cd6d5b986a128fcb0b96c2667ebac13db597b436
jakkujakku <jakkujakku98@gmail.com>
2023-08-31 20:09:04 +0900

    [FEAT]: ProgressListCell 생성 기능 추가

--- DiaryApp/Controllers/ProgressListViewController.swift
+++ DiaryApp/Controllers/ProgressListViewController.swift
@@ -6,12 +6,83 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProgressListViewController: UIViewController {
-    static let identifier = "ProgressListViewController"
+    private let collectionView = CustomCollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureCollectionView()
+        configureLayout()
+    }
+
+    private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = Const.itemSize
+        layout.minimumLineSpacing = Const.itemSpacing
+        layout.minimumInteritemSpacing = 0
+        return layout
+    }()
+
+    private func configureCollectionView() {
+        collectionView.register(ProgressListCell.self, forCellWithReuseIdentifier: ProgressListCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.isScrollEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = true
+        collectionView.backgroundColor = .clear
+        collectionView.clipsToBounds = true
+        collectionView.isPagingEnabled = false
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.contentInset = Const.collectionViewContentInset
+        collectionView.decelerationRate = .fast
+
+        collectionView.collectionViewLayout = collectionViewFlowLayout
+    }
+
+    private func configureLayout() {
+        view.addSubview(collectionView)
+
+        collectionView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
+        }
+    }
+}
+
+extension ProgressListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProgressListCell.identifier, for: indexPath) as? ProgressListCell else { return UICollectionViewCell() }
+        cell.backgroundColor = .systemRed
+        cell.layer.cornerRadius = 20
+        return cell
+    }
+}
+
+extension ProgressListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interItemSpacing: CGFloat = 10
+        let padding: CGFloat = 10
+        let width = (collectionView.bounds.width - interItemSpacing * 3 - padding * 2)
+        print("### \(width)")
+        let height = width / 3
+        return CGSize(width: width, height: height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
 }
---
+++ DiaryApp/Views/Cells/ProgressListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProgressListCell.swift
+//  DiaryApp
+//
+//  Created by (^ㅗ^)7 iMac on 2023/08/31.
+//
+
+import UIKit
+
+class ProgressListCell: UICollectionViewCell {
+    static let identifier = "ProgressListCell"
+}
