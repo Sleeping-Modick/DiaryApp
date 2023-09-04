bb40d632ddcdd66f1b12b069455765d76aa20f8a
김기현 <creeps1104@gmail.com>
2023-09-04 16:50:42 +0900

    [FEAT] TEST

--- DiaryApp/Resources/AppDelegate.swift
+++ DiaryApp/Resources/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import FirebaseCore
 import UIKit
-
+//김은경 짱 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
